a950c1aa92bea816c09b15cb04ce04f4fee3f2c4
Artur Brodzki <artur.brodzki@gmail.com>
2018-11-02 15:52:20 +0100

    cereal serialization + benchmarks

--- benchmark/Bench.hs
+++ benchmark/Bench.hs
@@ -13,5 +13,30 @@ module Main (
     main
 ) where
 
+import           Control.Monad.Trans.Class
+import           Control.Monad.Trans.Except
+import           Criterion.Main
+import           Multilinear.Generic
+import           Multilinear.Generic.Serialize
+import qualified Multilinear.Matrix         as Matrix
+
+gen :: Int -> Int -> Double
+gen j k = sin (fromIntegral j) + cos (fromIntegral k)
+
+writeMatrixBench :: Int -> Benchmark
+writeMatrixBench s = 
+    bench ((show s) ++ "x" ++ (show s)) $ 
+        nfIO $ toBinaryFile (Matrix.fromIndices "ij" s s gen) ("benchmark/binary-write-matrix-" ++ show s ++ ".csv")
+
+readMatrixBench :: Int -> Benchmark
+readMatrixBench s = do
+    let tensorDoubleIOT = fromBinaryFile ("benchmark/binary-write-matrix-" ++ show s ++ ".csv") :: ExceptT String IO (Tensor Double)
+    bench ((show s) ++ "x" ++ (show s)) $ 
+        nfIO $ runExceptT tensorDoubleIOT
+
+
 main :: IO ()
-main = putStrLn "Benchmark to do..."
+main = defaultMain [
+    bgroup "matrix binary write" $ writeMatrixBench <$> [100, 200, 400, 800, 1600, 3200],
+    bgroup "matrix binary read"  $ readMatrixBench  <$> [100, 200, 400, 800, 1600, 3200]
+    ]
--- src/Multilinear/Generic/Serialize.hs
+++ src/Multilinear/Generic/Serialize.hs
@@ -11,10 +11,7 @@ Portability : Windows/POSIX
 
 module Multilinear.Generic.Serialize (
     toBinary, toBinaryFile,
-    fromBinary, fromBinaryFile,
-    Multilinear.Generic.Serialize.toJSON, toJSONFile,
-    Multilinear.Generic.Serialize.fromJSON, fromJSONFile,
-    --fromCSV, toCSV
+    fromBinary, fromBinaryFile
 ) where
 
 import           Codec.Compression.GZip
@@ -58,12 +55,13 @@ toBinary = compress . Data.Serialize.encodeLazy
 {-| Write tensor to binary file. Uses compression with gzip -}
 toBinaryFile :: (
     Serialize a 
-  ) => String    -- ^ File name
-    -> Tensor a  -- ^ Tensor to serialize
+  ) => Tensor a  -- ^ Tensor to serialize
+    -> String    -- ^ File name
     -> IO ()
-toBinaryFile fileName = 
+toBinaryFile t fileName = do
+  let bs = toBinary t
   runConduitRes $ 
-    sourceLazy . bs .| sinkFile fileName
+    sourceLazy bs .| sinkFile fileName
 
 {-| Deserialize tensor from zlib compressed binary string -}
 fromBinary :: (
@@ -75,83 +73,9 @@ fromBinary = Data.Serialize.decodeLazy . decompress
 {-| Read tensor from binary file -}
 fromBinaryFile :: (
     Serialize a
-  ) => String                      -- ^ File path. 
-    -> Except String IO (Tensor a) -- ^ Deserialized tensor or an error message
+  ) => String                       -- ^ File path. 
+    -> ExceptT String IO (Tensor a) -- ^ Deserialized tensor or an error message
 fromBinaryFile fileName = do
-  contents <- lift $ runConduitRes $ sourceFile fileName .| sinkLazy
-  except $ fromBinary contents
-
-{-| Serialize tensor to JSON string -}
-toJSON :: (
-    ToJSON a
-  ) => Tensor a              -- ^ Tensor to serialize. 
-    -> ByteString.ByteString -- ^ Tensor serialized to lazy ByteString. 
-toJSON = Data.Aeson.encode
-
-{-| Write tensor to JSON file -}
-toJSONFile :: (
-    ToJSON a
-  ) => String   -- ^ File path. 
-    -> Tensor a -- ^ Tensor to serialize
-    -> IO ()
-toJSONFile name = ByteString.writeFile name . Multilinear.Generic.Serialize.toJSON
-
-{-| Deserialize tensor from JSON string -}
-fromJSON :: (
-    FromJSON a
-  ) => ByteString.ByteString -- ^ ByteString to deserialize
-    -> Maybe (Tensor a)      -- ^ Deserialized tensor or Nothing, if deserialization error occured. 
-fromJSON = Data.Aeson.decode
-
-{-| Read tensor from JSON file -}
-fromJSONFile :: (
-    FromJSON a
-  ) => String               -- ^ File path. 
-    -> MaybeT IO (Tensor a) -- ^ Deserialized tensor or Nothing, if error occured. 
-fromJSONFile name = do
-    contents <- lift $ ByteString.readFile name
-    MaybeT $ return $ Multilinear.Generic.Serialize.fromJSON contents
-
-{-| Read tensor (matrix) components from CSV file. -}
---{-# INLINE fromCSV #-}
-{-fromCSV :: (
-    Num a, Serialize a
-  ) => String                                  -- ^ Indices names (one character per index, first character: rows index, second character: columns index)
-    -> String                                  -- ^ CSV file name
-    -> Char                                    -- ^ Separator expected to be used in this CSV file
-    -> EitherT SomeException IO (Tensor a)     -- ^ Generated matrix or error message
-
-fromCSV x = case x of
-  [u,d] -> \fileName separator -> do
-    csv <- EitherT $ readCSVFile (CSVS separator (Just '"') (Just '"') separator) fileName
-    let components = (Data.Serialize.decode <$> ) <$> csv
-    let rows = length components
-    let columns = if rows > 0 then length $ rights (head components) else 0
-    if rows > 0 && columns > 0
-    then return $ 
-      FiniteTensor (Finite.Contravariant rows [u]) $ (
-        SimpleFinite (Finite.Covariant columns [d]) . Boxed.fromList . rights
-      ) <$> Boxed.fromList components
-    else EitherT $ return $ Left $ SomeException $ TypeError deserializationError
-
-  _ -> \_ _ -> return $ Err invalidIndices
--}
-
-{-| Write matrix to CSV file. -}
---{-# INLINE toCSV                    #-}
-{-toCSV :: (
-    Num a, Serialize a
-  ) => Tensor a  -- ^ Matrix to serialize. If given tensor os not a matrix, an error occurs and no data (0 rows) are saved to file. 
-    -> String    -- ^ CSV file name
-    -> Char      -- ^ Separator expected to be used in this CSV file
-    -> IO Int    -- ^ Number of rows written
-
-toCSV t = case order t of
-  (1,1) -> \fileName separator ->
-    let t' = _standardize t
-        elems = Boxed.toList $ Boxed.toList . tensorScalars <$> tensorsFinite t'
-        encodedElems = (Data.Serialize.encode <$>) <$> elems
-    in  writeCSVFile (CSVS separator (Just '"') (Just '"') separator) fileName encodedElems
-
-  _ -> \_ _ -> return 0
--}
+  contents <- lift $ runConduitRes $ 
+    sourceFile fileName .| sinkLazy
+  ExceptT $ return $ fromBinary contents
